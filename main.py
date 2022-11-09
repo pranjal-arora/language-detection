50b5f19fa8e77375fb42007bf71aa54f19080d16
pranjal-arora <prv.gma@gmail.com>
2022-11-09 14:21:23 +0530

    1

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/dataset.csv")
+print(data.head())
+data.isnull().sum()
+data["language"].value_counts()
+x = np.array(data["Text"])
+y = np.array(data["language"])
+
+cv = CountVectorizer()
+X = cv.fit_transform(x)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.33,random_state=42)
+model = MultinomialNB()
+model.fit(X_train,y_train)
+model.score(X_test,y_test)
+user = input("Enter a Text: ")
+data = cv.transform([user]).toarray()
+output = model.predict(data)
+print(output)
